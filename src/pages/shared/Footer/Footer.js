a2cb8f0176e918171be11d8c8a235596517655f5
PranoySarker <pranoysarker01@gmail.com>
2022-04-17 23:52:03 +0600

    protected route

--- src/pages/shared/Footer/Footer.js
+++ src/pages/shared/Footer/Footer.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-dark'>
-            <h2>This is footer</h2>
-        </div>
+        <div className='bg-dark text-white' style={{ height: 'auto' }}>
+            <div className='container pt-3'>
+                <div className='row gx-2'>
+                    <div className="col-6 ">
+                        <h5>DENTAL HEALTHCARE</h5>
+                        <p>Cosmetic Dentistry</p>
+                        <p>Endodontics</p>
+                        <p>Oral Surgery</p>
+                        <p>General Dental Care</p>
+                    </div>
+                    <div className='col-6 '>
+                        <h5>ADDRESS</h5>
+                        <p>3674 Harrison Street</p>
+                        <p>San Francisco, CA 94143, USA</p>
+                        <p>Mon–Fri: 8:00 am to 8:00 pmSat–Sun: 8:00 am to 4:00 pm</p>
+                    </div>
+                </div >
+            </div >
+        </div >
     );
 };
 
